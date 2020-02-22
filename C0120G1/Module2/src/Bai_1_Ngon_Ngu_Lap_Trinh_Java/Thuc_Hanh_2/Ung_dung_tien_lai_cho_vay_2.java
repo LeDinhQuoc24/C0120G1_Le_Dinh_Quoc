2603ee2fb65f139f9683569d9cf25e0ee1a1ca14
LeDinhQuoc24 <kudo2412@gmail.com>
2020-02-22 17:17:30 +0700

    addfile

---
+++ C0120G1/Module2/src/Bai_1_Ngon_Ngu_Lap_Trinh_Java/Thuc_Hanh_2/Ung_dung_tien_lai_cho_vay_2.java
@@ -0,0 +1,21 @@
+package Bai_1_Ngon_Ngu_Lap_Trinh_Java.Thuc_Hanh_2;
+
+import java.util.Scanner;
+
+public class Ung_dung_tien_lai_cho_vay_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập số tiền muốn gửi");
+        double tienGui = input.nextDouble();
+        System.out.println("Nhập tỉ lệ lãi");
+        double tiLeLai = input.nextDouble();
+        System.out.println("Nhập số tháng gửi");
+        int soThangGui = input.nextInt();
+        double soTienLai;
+        for (int i = 0; i < soThangGui; i++) {
+            soTienLai=tienGui*(tiLeLai/100)/12;
+            tienGui+=soTienLai;
+        }
+        System.out.println("Số tiền lãi nhận được "+ "soTienLai");
+    }
+}
