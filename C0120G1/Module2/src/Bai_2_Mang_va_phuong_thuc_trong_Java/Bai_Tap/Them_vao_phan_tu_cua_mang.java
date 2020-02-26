af54e3f6f408dd293be6d36659726eb8012dc125
LeDinhQuoc24 <kudo2412@gmail.com>
2020-02-26 16:54:08 +0700

    addfile

--- C0120G1/Module2/src/Bai_2_Mang_va_phuong_thuc_trong_Java/Bai_Tap/Them_vao_phan_tu_cua_mang.java
+++ C0120G1/Module2/src/Bai_2_Mang_va_phuong_thuc_trong_Java/Bai_Tap/Them_vao_phan_tu_cua_mang.java
@@ -16,6 +16,12 @@ public class Them_vao_phan_tu_cua_mang {
         } while (size > 20);
         array = new int[size];
         int index;
+        System.out.println("Hiển thị mảng tạo ngẫu nhiên");
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 100);
+            System.out.print(array[i]+"\t");
+        }
+        System.out.println("");
         do {
             System.out.println("Chọn vị trí phần tử cần gán giá trị");
             index = input.nextInt();
@@ -27,10 +33,20 @@ public class Them_vao_phan_tu_cua_mang {
             }
         } while (index > size||index<1);
         System.out.println("Chọn giá trị cần gán");
-        array[index-1] = input.nextInt();
+        int value = input.nextInt();
         System.out.println("Hiển thị các phần tử của mảng");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+"\t");
+        int[]array2=new int[size+1];
+        for (int i = 0; i < size+1; i++) {
+            if (i < (index - 1)) {
+                array2[i] = array[i];
+            } else if (i == (index - 1)) {
+                array2[i]=value;
+            }else{
+                array2[i] = array[i - 1];
+            }
+        }
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i]+"\t");
         }
     }
 }
