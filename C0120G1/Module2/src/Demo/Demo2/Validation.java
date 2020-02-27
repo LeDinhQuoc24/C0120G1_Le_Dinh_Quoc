8aab4a8a457e0375ec3d5949a3c58f104a0294f8
Le Dinh Quoc <kudo2412@gmail.com>
2020-02-27 20:27:06 +0700

    addfile

---
+++ C0120G1/Module2/src/Demo/Demo2/Validation.java
@@ -0,0 +1,17 @@
+package Demo.Demo2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Validation {
+    public static int checkAndGetNumber(String contentError) {
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                return sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(contentError);
+            }
+        }
+    }
+}
