55798c584a39449f47e156a46d8a7c405e56ec23
LeDinhQuoc24 <kudo2412@gmail.com>
2020-02-28 10:55:07 +0700

    addfile

--- C0120G1/Module2/src/Bai_2_Mang_va_phuong_thuc_trong_Java/Bai_Tap/Tinh_tong_cac_so_o_duong_cheo_chinh_cua_ma_tran_vuong.java
+++ C0120G1/Module2/src/Bai_2_Mang_va_phuong_thuc_trong_Java/Bai_Tap/Tinh_tong_cac_so_o_duong_cheo_chinh_cua_ma_tran_vuong.java
@@ -17,7 +17,9 @@ public class Tinh_tong_cac_so_o_duong_cheo_chinh_cua_ma_tran_vuong {
         System.out.println("Hiển thị ma trận tạo thành ngẫu nhiên");
         array = new int[size][size];
         String ketQua = "";
+        String ketQua2 = "";
         int sum=0;
+        int sum2=0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 array[i][j] = (int) (Math.random() * 15);
@@ -27,11 +29,23 @@ public class Tinh_tong_cac_so_o_duong_cheo_chinh_cua_ma_tran_vuong {
                     sum += array[i][j];
                 }
             }
+
             System.out.println("");
         }
+        for (int i = 0; i < array.length; i++) {
+            ketQua2 += array[i][array.length -1- i]+"\t";
+            sum+=array[i][array.length-1-i];
+        }
+
+
+
         System.out.println("Hiển thị  các phần tử ở đường chéo chính");
         System.out.println(ketQua);
         System.out.println("Hiển thị tổng các phần tử ở đường chéo chính");
         System.out.println(sum);
+        System.out.println("Hiển thị các phần tử ở đường chéo phụ");
+        System.out.println(ketQua2);
+        System.out.println("Hiển thị tổng các phần tử ở đường chéo phụ");
+        System.out.println(sum2);
     }
 }
