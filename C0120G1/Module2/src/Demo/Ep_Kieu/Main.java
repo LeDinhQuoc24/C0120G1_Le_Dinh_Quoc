2ffb701383aff9f86d16b5ba5577f099d5633c76
Le Dinh Quoc <kudo2412@gmail.com>
2020-03-04 18:03:06 +0700

    addfile

--- C0120G1/Module2/src/Demo/Ep_Kieu/Main.java
+++ C0120G1/Module2/src/Demo/Ep_Kieu/Main.java
@@ -8,13 +8,13 @@ public class Main {
         ((CustomerChild)customer).setAge("99");
         customerChild.setAge("17");
         customerChild.setName("quoc");
-<<<<<<< HEAD
+
         ((CustomerChild)customer).setAge("20");
         customerChild=(CustomerChild)customer;
-=======
+
         customerChild=(CustomerChild)customer;
         System.out.println(customer);
->>>>>>> 875a6e46651cb7d950b501942d4e4fed62ba074f
+
         System.out.println(customerChild);
         System.out.println(customer);
     }
