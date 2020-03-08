7bf94e9f4460aba59277722d25436580bf09ae12
LeDinhQuoc24 <kudo2412@gmail.com>
2020-03-08 13:10:43 +0700

    addfile

--- C0120G1/Module2/src/CaseStudyversion2/controller/X6_ShowBookingCustomer.java
+++ C0120G1/Module2/src/CaseStudyversion2/controller/X6_ShowBookingCustomer.java
@@ -3,20 +3,22 @@ package CaseStudyversion2.controller;
 import CaseStudyversion2.commons.Comparator;
 import CaseStudyversion2.models.Customer;
 
-import java.util.ArrayList;
+
 import java.util.Collections;
 
-import static CaseStudyversion2.commons.FuncWriteAndReadFileCSV.readFileX;
+
+import static CaseStudyversion2.commons.SaveAndLoadBooking.loadBooking;
+import static CaseStudyversion2.views.MainRun.listBooking;
 
 
 public class X6_ShowBookingCustomer {
     public static void showBookingCustomer() {
-        ArrayList<Customer> listCustomer=readFileX("F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data\\saveBooking.csv" );
+        loadBooking();
         System.out.println("Load file Information Booking successfully");
         System.out.println("\nxxxxxxxxxxxxx List Booking xxxxxxxxxxxxxxx\n\n");
         Comparator comparator = new Comparator();
-        Collections.sort(listCustomer,comparator);
-        for (Customer customer : listCustomer) {
+        Collections.sort(listBooking,comparator);
+        for (Customer customer : listBooking) {
                 System.out.println(customer.showInfo()+"\n"+
                         "************Register Service**************\n"
                         +customer.getService().showInfo()+"\n");
