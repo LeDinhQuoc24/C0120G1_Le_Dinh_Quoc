0dbaf24df1f94699025c61055e722006ad63d7bf
LeDinhQuoc24 <kudo2412@gmail.com>
2020-03-11 13:18:14 +0700

    addfile

--- C0120G1/Module2/src/CaseStudyversion2/views/MainRun.java
+++ C0120G1/Module2/src/CaseStudyversion2/views/MainRun.java
@@ -16,12 +16,12 @@ public class MainRun {
     static public ArrayList<Room> listRoom = new ArrayList<>();
     static public ArrayList<Villa> listVilla = new ArrayList<>();
     static public ArrayList<Employee> listEmployee = new ArrayList<>();
-    static public String pathVilla="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveVilla.csv";
-    static public String pathHouse="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveHouse.csv";
-    static public String pathRoom="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveRoom.csv";
-    static public String pathCustomer="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveCustomer.csv";
-    static public String pathBooking="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveBooking.csv";
-    static public String pathEmployee="F:\\ProjectCodyGym\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveEmployee.csv";
+    static public String pathVilla="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveVilla.csv";
+    static public String pathHouse="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveHouse.csv";
+    static public String pathRoom="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveRoom.csv";
+    static public String pathCustomer="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveCustomer.csv";
+    static public String pathBooking="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveBooking.csv";
+    static public String pathEmployee="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveEmployee.csv";
     public static void main(String[] args) {
         displayMenu();
     }
