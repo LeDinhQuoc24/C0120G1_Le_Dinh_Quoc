da4aa6153b00ed62af8e42c6e5b29dd533b00cc9
LeDinhQuoc24 <kudo2412@gmail.com>
2020-03-24 14:37:20 +0700

    addfile

--- C0120G1/Module2_Javacore/src/CaseStudyversion2/views/MainRun.java
+++ C0120G1/Module2_Javacore/src/CaseStudyversion2/views/MainRun.java
@@ -19,13 +19,13 @@ public class MainRun {
     static public ArrayList<Villa> listVilla = new ArrayList<>();
     static public ArrayList<Employee> listEmployee = new ArrayList<>();
 
-    static public String pathVilla="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveVilla.csv";
-    static public String pathHouse="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveHouse.csv";
-    static public String pathRoom="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveRoom.csv";
-    static public String pathCustomer="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveCustomer.csv";
-    static public String pathBooking="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveBooking.csv";
-    static public String pathEmployee="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveEmployee.csv";
-    static public String pathViewer="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveViewer.csv";
+    static public String pathVilla="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveVilla.csv";
+    static public String pathHouse="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveHouse.csv";
+    static public String pathRoom="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveRoom.csv";
+    static public String pathCustomer="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveCustomer.csv";
+    static public String pathBooking="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveBooking.csv";
+    static public String pathEmployee="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveEmployee.csv";
+    static public String pathViewer="F:\\ProjectCodyGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2_Javacore\\src\\CaseStudyversion2\\data2\\saveViewer.csv";
     static public ArrayList<Viewer> listViewer = new ArrayList<>();
 //    static public String pathVilla="D:\\CodeGym\\ProjectCodeGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveVilla.csv";
 //    static public String pathHouse="D:\\CodeGym\\ProjectCodeGym\\C0120G1_Le_Dinh_Quoc\\C0120G1\\Module2\\src\\CaseStudyversion2\\data2\\saveHouse.csv";
