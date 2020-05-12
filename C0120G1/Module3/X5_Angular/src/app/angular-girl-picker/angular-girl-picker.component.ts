ab2e75db26af26548acf64cfc4bbd9aa53d90d6e
Le Dinh Quoc <kudo2412@gmail.com>
2020-05-12 22:44:47 +0700

    addfile

--- C0120G1/Module3/X5_Angular/src/app/angular-girl-picker/angular-girl-picker.component.ts
+++ C0120G1/Module3/X5_Angular/src/app/angular-girl-picker/angular-girl-picker.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./angular-girl-picker.component.css']
 })
 export class AngularGirlPickerComponent implements OnInit {
-  girlName = 'HuaLuuLiem';
+  girlName = 'Hứa Lưu Liễm';
   girlImage = 'assets/x1.png';
   constructor() { }
   updateName(name) {
@@ -17,6 +17,23 @@ export class AngularGirlPickerComponent implements OnInit {
   }
   onSelectChange(value) {
     this.girlImage = value;
+
+  }
+  calculate(value) {
+    switch (value) {
+      case 'assets/x1.png':
+        this.girlName = 'Hứa Lưu Liễm';
+        break;
+      case 'assets/x2.png':
+        this.girlName = 'Hạ Vi Lương';
+        break;
+      case 'assets/x3.png':
+        this.girlName = 'Diệp Hoàng Trúc Mai';
+        break;
+      case 'assets/x4.png':
+        this.girlName = 'Văn Nhân Mục Nguyệt';
+        break;
+    }
   }
 
   ngOnInit() {
