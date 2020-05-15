0ea142da59ac848d6bb25978332405963af47fdd
LeDinhQuoc24 <kudo2412@gmail.com>
2020-05-15 15:25:33 +0700

    addfile

--- C0120G1/Module3/X6-Component-Template/src/app/angular-countdown/angular-countdown.component.ts
+++ C0120G1/Module3/X6-Component-Template/src/app/angular-countdown/angular-countdown.component.ts
@@ -1,16 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-angular-countdown',
   templateUrl: './angular-countdown.component.html',
   styleUrls: ['./angular-countdown.component.css']
 })
-export class AngularCountdownComponent implements OnInit {
-  
+export class AngularCountdownComponent implements OnInit, OnDestroy {
+  private intervalId = 0;
+  message = '';
+  remainingTime: number;
 
-  constructor() { }
+  @Input() seconds = 11;
+
+
+  constructor() {
+  }
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
 
   ngOnInit() {
+    this.reset();
+    this.start();
+
   }
+
+  ngOnDestroy() {
+    this.clearTimer();
+  }
+
+  start() {
+    this.countDown();
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.seconds;
+    }
+  }
+
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.remainingTime} seconds`;
+  }
+
+  reset() {
+    this.clearTimer();
+    this.remainingTime = this.seconds;
+    this.message = `Click start button to start the Countdown`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime === 0) {
+        this.message = 'Blast off!';
+        this.clearTimer();
+      } else {
+        this.message = `T-${this.remainingTime} seconds and counting`;
+      }
+    }, 1000);
+  }
+
 
 }
